cd063d2262f7271c74588b656429123698a9c893
Tatiana <bangchanka0310@gmail.com>
2025-08-22 20:49:15 +0300

    add corrections

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,12 +1,52 @@
+import {
+  loginUser,
+  logoutUser,
+  refreshUserSession,
+  registerUser,
+} from "../services/auth";
+
 export async function registerController(request, response) {
   const user = await registerUser(request.body);
 
   response.json({
-    status: 201,
-    message: "User registered successfully",
-    data: user,
+    id: user._id,
+    email: user.email,
   });
 }
-export async function loginController(request, response) {}
-export async function refreshUserSessionController(request, response) {}
-export async function logoutController(request, response) {}
+export async function loginController(request, response) {
+  const session = await loginUser(request.body.email, request.body.password);
+
+  response.cookies("sessionId", session._id, {
+    httpOnly: true,
+    expire: session.refreshTokenValidUntil,
+  });
+
+  response.cookies("refreshToken", session._id, {
+    httpOnly: true,
+    expire: session.refreshTokenValidUntil,
+  });
+
+  response.json({
+    data: {
+      accessToken: session.accessToken,
+      refreshToken: session.refreshToken,
+    },
+    email: session.email,
+    password: session.password,
+  });
+}
+export async function refreshUserSessionController(request, response) {
+  const { sessionId, refreshToken } = request.cookies;
+  await refreshUserSession();
+}
+export async function logoutController(request, response) {
+  const { sessionId } = request.cookies;
+  if (typeof sessionId !== "undefined") {
+    await logoutUser(sessionId);
+  }
+
+  response.clearCookie("sessionId");
+  response.clearCookie("refreshToken");
+
+  response.status(204).end();
+}
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,20 +1,63 @@
+import crypto from "node:crypto";
+
 import createHttpError from "http-errors";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import User from "../models/user.js";
+import Session, { sessionModel } from "../models/session.js";
+import { refreshUserSessionController } from "../controllers/auth.js";
 
 export async function registerUser(payload) {
-  const { email, password, name } = payload;
-  if (!email || !password || !name) {
-    throw new createHttpError.BadRequest("Missing required fields");
-  }
-  const existingUser = await User.findOne({ email: payload.email });
+  const user = await User.findOne({ email: payload.email });
   if (user !== null) {
     throw new createHttpError.Conflict("Email is already in use");
   }
   payload.password = await bcrypt.hash(payload.password, 10);
   return User.create(payload);
+
+  // const { accessToken, refreshToken } = generateTokens(newUser);
+  // return { id: newUser._id email: newUser.email, accessToken, refreshToken};
 }
-export async function loginUser(payload) {}
-export async function refreshUserSession(payload) {}
-export async function logoutUser(session_id) {}
+export async function loginUser(email, password) {
+  const user = await User.findOne({ email });
+
+  if (user === null) {
+    throw new createHttpError.Unauthorized("Email or password is incorrect");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (isMatch !== true) {
+    throw new createHttpError.Unauthorized("Email or password is incorrect");
+  }
+
+  await Session.deleteOne({ userId: user._id });
+
+  return Session.create({
+    userId: user._id,
+    accessToken: crypto.randomBytes(30).toString("base64"),
+    refreshToken: crypto.randomBytes(30).toString("base64"),
+    accessTokenValidUntil: new Date(Date.now() + 10 * 60 * 1000),
+    refreshTokenValidUntil: new Date(Date.now() + 24 * 60 * 60 * 1000),
+  });
+}
+export async function refreshUserSession(sessionId, refreshToken) {}
+export async function logoutUser(session_id) {
+  await sessionModel.deleteOne({ _id: session_id });
+}
+
+// function generateTokens(user) {
+//   const accessToken = jwt.sign(
+//     { id: user._id },
+//     process.env.ACCESS_TOKEN_SECRET,
+//     { expiresIn: "15m" }
+//   );
+//   const refreshToken = jwt.sign(
+//     { id: user._id },
+//     process.env.REFRESH_TOKEN_SECRET,
+//     { expiresIn: "7d" }
+//   );
+
+//   return { accessToken, refreshToken };
+// }
