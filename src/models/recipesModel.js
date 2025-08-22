34a925e1998f139f3e3b5f32c4c8021f6cd44297
Oleksiy <Unialex@ukr.net>
2025-08-22 20:09:32 +0300

    Add recipes model

--- src/models/recipesModel.js
+++ src/models/recipesModel.js
@@ -1,66 +1,31 @@
-import { model, Schema } from "mongoose";
+import { model, Schema } from 'mongoose';
 
 const recipesSchema = new Schema(
   {
-    title: {
-      type: String,
-      required: true,
-    },
-    category: {
-      type: String,
-      // ref: "Category",
-      required: true,
-    },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "Users",
-      required: true,
-    },
-    area: {
-      type: Schema.Types.ObjectId,
-      ref: "Areas",
-      required: true,
-    },
-    instruction: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    calories: {
-      type: Number,
-      required: false,
-      default: null,
-    },
-    thumb: {
-      type: String,
-      required: false,
-      default: null,
-    },
-    time: {
-      type: Number,
-      required: true,
-    },
+    title: { type: String, required: true },
+    category: { type: String, required: true },
+    owner: { type: Schema.Types.ObjectId, ref: 'Users', required: true },
+    area: { type: Schema.Types.ObjectId, ref: 'Areas', required: true },
+    instruction: { type: String, required: true },
+    description: { type: String, required: true },
+    calories: { type: Number, default: null },
+    thumb: { type: String, default: null },
+    time: { type: Number, required: true },
     ingredients: [
       new Schema(
         {
-          id: {
-            type: Schema.Types.ObjectId,
-            ref: "Ingredient",
-            required: true,
-          },
+          id: { type: Schema.Types.ObjectId, ref: 'Ingredient', required: true },
           measure: { type: String, required: true },
         },
-        { _id: false }
+        { _id: false },
       ),
     ],
   },
   {
     timestamps: true,
     versionKey: false,
-  }
+  },
 );
 
-export const recipesCollection = model("Recipes", recipesSchema);
+recipesSchema.index({ name: 'text' });
+export const recipesCollection = model('Recipes', recipesSchema);
