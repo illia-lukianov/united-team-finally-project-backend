b7813e431c00b27c9175ff7bf4d50078f59f1a87
Tatiana <bangchanka0310@gmail.com>
2025-08-25 15:51:03 +0300

    fix all remaining bugs

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,53 +1,50 @@
-import { User } from "../models/user.js";
-import {
-  loginUser,
-  logoutUser,
-  refreshUserSession,
-  registerUser,
-} from "../services/auth.js";
+import { userModel } from '../models/user.js';
+import { loginUser, logoutUser, refreshUserSession, registerUser } from '../services/auth.js';
 
 export async function registerController(request, response) {
   const user = await registerUser(request.body);
-  console.log("User created:", user);
   const session = await loginUser(request.body.email, request.body.password);
-  console.log("Session created:", session);
 
-  response.cookie("sessionId", session._id, {
+  response.cookie('sessionId', session._id, {
     httpOnly: true,
     expire: session.refreshTokenValidUntil,
   });
 
-  response.cookie("refreshToken", session.refreshToken, {
+  response.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
     expire: session.refreshTokenValidUntil,
   });
 
   response.json({
     status: 201,
-    message: "Successfully registered",
-    data: user,
-    id: user._id,
-    email: user.email,
-    accessToken: session.accessToken,
-    refreshToken: session.refreshToken,
+    message: 'Successfully registered',
+    data: {
+      user: {
+        id: user._id,
+        email: user.email,
+      },
+      accessToken: session.accessToken,
+    },
   });
 }
 export async function loginController(request, response) {
   const session = await loginUser(request.body.email, request.body.password);
 
-  const user = await User.findById(session.userId);
+  const user = await userModel.findById(session.userId);
 
-  response.cookie("sessionId", session._id, {
+  response.cookie('sessionId', session._id, {
     httpOnly: true,
     expire: session.refreshTokenValidUntil,
   });
 
-  response.cookie("refreshToken", session.refreshToken, {
+  response.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
     expire: session.refreshTokenValidUntil,
   });
 
   response.json({
+    status: 201,
+    message: 'User successfully registered and login',
     data: {
       accessToken: session.accessToken,
       refreshToken: session.refreshToken,
@@ -63,12 +60,12 @@ export async function refreshUserSessionController(request, response) {
   const { sessionId, refreshToken } = request.cookies;
   const session = await refreshUserSession(sessionId, refreshToken);
 
-  response.cookie("sessionId", session._id, {
+  response.cookie('sessionId', session._id, {
     httpOnly: true,
     expire: session.refreshTokenValidUntil,
   });
 
-  response.cookie("refreshToken", session.refreshToken, {
+  response.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
     expire: session.refreshTokenValidUntil,
   });
@@ -82,12 +79,12 @@ export async function refreshUserSessionController(request, response) {
 }
 export async function logoutController(request, response) {
   const { sessionId } = request.cookies;
-  if (typeof sessionId !== "undefined") {
+  if (typeof sessionId !== 'undefined') {
     await logoutUser(sessionId);
   }
 
-  response.clearCookie("sessionId");
-  response.clearCookie("refreshToken");
+  response.clearCookie('sessionId');
+  response.clearCookie('refreshToken');
 
   response.status(204).end();
 }
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,47 +1,41 @@
-import crypto from "node:crypto";
+import crypto from 'node:crypto';
 
-import createHttpError from "http-errors";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import getEnvVariables from "../utils/getEnvVariables.js";
+import createHttpError from 'http-errors';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import getEnvVariables from '../utils/getEnvVariables.js';
 
-import { User } from "../models/user.js";
-import { sessionModel } from "../models/session.js";
+import { userModel } from '../models/user.js';
+import { sessionModel } from '../models/session.js';
 
 export async function registerUser(payload) {
-  const user = await User.findOne({ email: payload.email });
+  const user = await userModel.findOne({ email: payload.email });
   if (user !== null) {
-    throw new createHttpError.Conflict("Email is already in use");
+    throw new createHttpError.Conflict('Email is already in use');
   }
   const hashedPassword = await bcrypt.hash(payload.password, 10);
 
-  return User.create({ ...payload, password: hashedPassword });
+  return userModel.create({ ...payload, password: hashedPassword });
 }
 export async function loginUser(email, password) {
-  const user = await User.findOne({ email });
+  const user = await userModel.findOne({ email });
 
   if (user === null) {
-    throw new createHttpError.Unauthorized("Email or password is incorrect");
+    throw new createHttpError.Unauthorized('Email or password is incorrect');
   }
 
   const isMatch = await bcrypt.compare(password, user.password);
 
   if (isMatch !== true) {
-    throw new createHttpError.Unauthorized("Email or password is incorrect");
+    throw new createHttpError.Unauthorized('Email or password is incorrect');
   }
 
-  const accessToken = jwt.sign(
-    { userId: user._id, email: user.email },
-    getEnvVariables("SECRET_JWT"),
-    { expiresIn: "10m" }
-  );
-  const refreshToken = jwt.sign(
-    { userId: user._id },
-    getEnvVariables("SECRET_JWT"),
-    {
-      expiresIn: "7d",
-    }
-  );
+  const accessToken = jwt.sign({ userId: user._id, email: user.email }, getEnvVariables('SECRET_JWT'), {
+    expiresIn: '10m',
+  });
+  const refreshToken = jwt.sign({ userId: user._id }, getEnvVariables('SECRET_JWT'), {
+    expiresIn: '7d',
+  });
 
   await sessionModel.deleteOne({ userId: user._id });
 
@@ -57,33 +51,27 @@ export async function refreshUserSession(sessionId, refreshToken) {
   const session = await sessionModel.findById(sessionId);
 
   if (session === null) {
-    throw new createHttpError.Unauthorized("Session not found");
+    throw new createHttpError.Unauthorized('Session not found');
   }
   if (session.refreshToken !== refreshToken) {
-    throw new createHttpError.Unauthorized("Refresh token us invalid");
+    throw new createHttpError.Unauthorized('Refresh token us invalid');
   }
   if (session.refreshTokenValidUntil < new Date()) {
-    throw new createHttpError.Unauthorized("Refresh token is expired");
+    throw new createHttpError.Unauthorized('Refresh token is expired');
   }
 
-  const user = await User.findById(session.userId);
+  const user = await userModel.findById(session.userId);
 
   if (user === null) {
-    throw new createHttpError.Unauthorized("User not found");
+    throw new createHttpError.Unauthorized('User not found');
   }
 
-  const newAccessToken = jwt.sign(
-    { userId: user._id, email: user.email },
-    getEnvVariables("SECRET_JWT"),
-    { expiresIn: "10m" }
-  );
-  const newRefreshToken = jwt.sign(
-    { userId: user._id },
-    getEnvVariables("SECRET_JWT"),
-    {
-      expiresIn: "7d",
-    }
-  );
+  const newAccessToken = jwt.sign({ userId: user._id, email: user.email }, getEnvVariables('SECRET_JWT'), {
+    expiresIn: '10m',
+  });
+  const newRefreshToken = jwt.sign({ userId: user._id }, getEnvVariables('SECRET_JWT'), {
+    expiresIn: '7d',
+  });
 
   await sessionModel.deleteOne({ _id: session._id });
 
