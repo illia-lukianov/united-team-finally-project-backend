248d57e5e50577fc9061606e73e75153fc97752c
Manches <Lukjanovilly@gmail.com>
2025-08-27 22:36:10 +0300

    fix-cookies

--- src/server.js
+++ src/server.js
@@ -11,45 +11,53 @@ import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import { UPLOAD_DIR } from './constants/index.js';
 import path from 'node:path';
 
-const PORT = getEnvVariables("PORT") ?? "3000";
-const allowedOrigins = [
-'http://localhost:5173',
-'https://united-team-finally-project-front-e.vercel.app',
-];
+const PORT = getEnvVariables('PORT') ?? '3000';
+const allowedOrigins = ['http://localhost:5173', 'https://united-team-finally-project-front-e.vercel.app'];
 
 export default function setupServer() {
   const app = express();
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// //   credentials: true,
-// // }));
+  // app.use(
+  //   cors({
+  //     origin: function (origin, callback) {
+  // if (!origin || allowedOrigins.includes(origin)) {
+  //   callback(null, true);
+  // } else {
+  //   callback(new Error('Not allowed by CORS'));
+  // }
+  // 'https://united-team-finally-project-front-e.vercel.app';
+  // },
+  //     credentials: true,
+  //   }),
+  // );
+
   app.use(
     cors({
-      origin: (origin, callback) => {
-        callback(null, origin || '*'); // дозволяє будь-який домен
-      },
+      origin: 'https://united-team-finally-project-front-e.vercel.app',
       credentials: true,
     }),
   );
+
+  // app.use(
+  //   cors({
+  //     origin: (origin, callback) => {
+  //       callback(null, origin || '*'); // дозволяє будь-який домен
+  //     },
+  //     credentials: true,
+  //   }),
+  // );
   app.use(express.json());
   app.use(
     pino({
       transport: {
-        target: "pino-pretty",
+        target: 'pino-pretty',
       },
-      level: "error",
-    })
+      level: 'error',
+    }),
   );
   app.use('/auth/uploads', express.static(UPLOAD_DIR));
   app.use(cookieParser());
-  app.use("/api-docs", swaggerDocs());
+  app.use('/api-docs', swaggerDocs());
   app.use('/thumb', express.static(path.resolve('src/uploads/photo')));
   app.use(router);
   app.use(errorHandler);
