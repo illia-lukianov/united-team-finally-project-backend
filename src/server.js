0ec231fdaac8e2459b2429daf029435b5638aab5
Lukianov Illia <Lukjanovilly@gmail.com>
2025-08-27 20:20:43 +0300

    update cors

--- src/server.js
+++ src/server.js
@@ -20,16 +20,17 @@ const allowedOrigins = [
 export default function setupServer() {
   const app = express();
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+// }));
+  app.use(cors());
   app.use(express.json());
   app.use(
     pino({
