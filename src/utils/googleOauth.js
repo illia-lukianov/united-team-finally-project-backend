067cf7f2cdac02645ad841458056c16ea2fccee3
Lukianov Illia <Lukjanovilly@gmail.com>
2025-09-01 18:09:41 +0300

    delete-comment

--- src/utils/googleOauth.js
+++ src/utils/googleOauth.js
@@ -1,24 +1,3 @@
-// import { OAuth2Client } from 'google-auth-library';
-// import getEnvVariables from './getEnvVariables.js';
-// const googleOauth2Client = new OAuth2Client({
-//  client_id: getEnvVariables('GOOGLE_CLIENT_ID'),
-//  client_secret: getEnvVariables('GOOGLE_CLIENT_SECRET'),
-//  redirectUri: getEnvVariables('GOOGLE_REDIRECT_URI'),
-// });
-
-// export async function getOAuthURL() {
-//   return googleOauth2Client.generateAuthUrl({
-//     scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'],
-//   });
-// }
-
-// export async function validateCode(code) {
-//   const response = await googleOauth2Client.getToken(code);
-//   return googleOauth2Client.verifyIdToken({
-//     idToken: response.tokens.id_token,
-//   });
-// }
-
 import { OAuth2Client } from 'google-auth-library';
 import getEnvVariables from './getEnvVariables.js';
 
@@ -30,8 +9,8 @@ const googleOauth2Client = new OAuth2Client({
 
 export async function getOAuthURL() {
   return googleOauth2Client.generateAuthUrl({
-    access_type: 'offline', // щоб отримати refresh token
-    prompt: 'consent',       // змушує показати вікно авторизації кожного разу
+    access_type: 'offline',
+    prompt: 'consent',       
     scope: [
       'https://www.googleapis.com/auth/userinfo.email',
       'https://www.googleapis.com/auth/userinfo.profile'
