964268065ff5998736756f182b57d53b7989dc18
Lukianov Illia <Lukjanovilly@gmail.com>
2025-09-02 08:44:38 +0300

    switch-to-english-lang-area

--- src/utils/getRegionByCoords.js
+++ src/utils/getRegionByCoords.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = process.env.LOCATIONIQ_KEY; 
+const API_KEY = process.env.LOCATIONIQ_KEY;
 
 export async function getRegionByCoords(lat, lng) {
   if (lat === undefined || lng === undefined) {
@@ -14,8 +14,10 @@ export async function getRegionByCoords(lat, lng) {
         lat,
         lon: lng,
         format: "json",
+        "accept-language": "en",
       },
     });
+
     return `${data.address.country}, ${data.address.state}`;
   } catch (err) {
     console.error("Reverse geocoding failed:", err.response?.data || err.message);
